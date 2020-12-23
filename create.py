557f3f9f656b1694ac06d8c9cf8c20015bc651e5
satoooon8888 <samidare256@gmail.com>
2020-12-23 20:56:26 +0900

    upgrade challenge

--- create.py
+++ create.py
@@ -1,33 +1,43 @@
-from random import randint
+import random
 
 def create_dummy():
+	op = random.choice(["mov", "add", "sub", "cmp"])
+	reg = random.choice(["eax", "ebx", "ecx", "edx"])
+	val = random.randint(0, 0x100000000)
+	asm = 'asm(".intel_syntax noprefix");\n'
+	asm += f'asm("{op} {reg}, {val}");\n'
+	asm += 'asm(".att_syntax");\n'
+	return asm
 	# return ""
-	return f'asm("mov ${randint(0, 0xffffffff)}, %r10");'
 
 flag = b"xm4s{mad_dummy_blocks_the_way!}"
+dummy_len = 8000
+
+if len(flag) % 4 != 0:
+	flag += b"\x00" * (4 - len(flag) % 4)
 
 code = """
 #include<stdio.h>
 int main(void) {
 """
 
-for i in range(2000 + randint(0, 1000)):
+for i in range(random.randint(dummy_len, dummy_len+1000)):
 	code += create_dummy()
 
-code += f"int fail=0;"
-code += f"printf(\"FLAG: \");"
-code += f"char buf[{len(flag)+1}];"
-for i in range(2000 + randint(0, 1000)):
+code += f"int fail=0;\n"
+code += f"printf(\"FLAG: \");\n"
+code += f"char buf[{len(flag)+1}];\n"
+for i in range(random.randint(dummy_len, dummy_len+1000)):
 	code += create_dummy()
 
-code += f'scanf("%{len(flag)}s%*c", buf);'
+code += f'scanf("%{len(flag)}s%*c", buf);\n'
 
-for i in range(2000 + randint(0, 1000)):
+for i in range(random.randint(dummy_len, dummy_len+1000)):
 	code += create_dummy()
 
-for i, fi in enumerate(flag):
-	code += f'if (buf[{i}] != {fi}) fail=1;'
-	for i in range(2000 + randint(0, 1000)):
+for i in range(0, len(flag), 4):
+	code += f'if (*(int*)(&buf[{i}]) != {int.from_bytes(flag[i:i+4], "little")}) fail=1;\n'
+	for i in range(random.randint(dummy_len, dummy_len+1000)):
 		code += create_dummy()
 
 code += """
--- view.sh
+++ view.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-objdump -d dummy -M intel | grep -v "mov    r10," | grep -v "movabs r10," | grep -v "00 00 00"  | grep -Po "(?<=cmp    al\,)0x[0-f]{2}" | python3 -c 'lines = open(0); [print(chr(int(li[2:4], 16)), end="") for li in lines]'
+objdump -d dummy -M intel \
+| grep -Pv "(add|sub|mov|cmp)    e.x," \
